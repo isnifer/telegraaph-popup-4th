5891ca7ec1fe6fed8b2e6c0ac76dea65f587cd08
Anton Kuznetsov <isnifer@gmail.com>
2014-02-11 13:11:34 +0400

    Added example with 20 buttons

--- js/common.js
+++ js/common.js
@@ -18,20 +18,20 @@
     };
 
     var popup = $('.popup'),
-        button = $('.b-main__button'),
-        // buttons = $$('.b-main__button'),
+        // button = $('.b-main__button'),
+        buttons = $$('.b-main__button'),
         closeElements = $$('.popup__close');
 
-    button.addEventListener('click', function () {
+    /*button.addEventListener('click', function () {
         popup.classList.toggle('popup_state_visible');
-    }, false);
+    }, false);*/
 
     // Example for 1000 buttons  
-    /* for (var j=0; j < buttons.length; j++) {
+    for (var j=0; j < buttons.length; j++) {
         buttons[j].addEventListener('click', function () {
             popup.classList.toggle('popup_state_visible');
         }, false);
-    } */
+    }
 
     for (var i=0; i < closeElements.length; i++) {
         closeElements[i].addEventListener('click', function (e) {
@@ -39,7 +39,7 @@
                 e.preventDefault();
                 popup.classList.toggle('popup_state_visible');
             }
-        }, false);
+        }, true);
     }
 
 }());
